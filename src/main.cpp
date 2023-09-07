d8ea2e257cf58e862a983d455062eb1b214145a5
ByJuanDiego <juan.castro.p@utec.edu.pe>
2023-09-07 17:42:46 -0500

    bug-fix: Bug quivering in fear: "Enriquefft, please spare us!" (but he didn't).

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
             "./index/",
             "metadata.json",
             "my_index.dat",
-            1,
+            200,
             get_expected_bucket_capacity<Record>(),
             true
     );
@@ -26,14 +26,19 @@ int main() {
     StaticHash<const char*, Record, decltype(equal_to), sha256<const char*>> static_hash(property, get_indexed_field, equal_to);
 
     // insert records
-    for (int i = 1; i < 3; i++) {
+    for (int i = 1; i < 100'000; i++) {
         std::string s = std::string("P") + std::to_string(i);
         Record record(i, s.c_str(), 15);
         static_hash.insert(record);
     }
 
-    for (const Record& record: static_hash.search("P1")) {
-        std::cout << record << std::endl;
+    // search records
+    for (int i = 1; i < 100'000; i++) {
+        std::string s = std::string("P") + std::to_string(i);
+
+        for (Record& record: static_hash.search(s.c_str())) {
+            std::cout << record << std::endl;
+        }
     }
 
     return EXIT_SUCCESS;
